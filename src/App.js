3744f4f6cc4e2e277383793e0db8f87f656851b1
wangshumin <wangshumin@dianwoba.com>
2019-02-25 16:43:55 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import Input from './component/Input'
+import 'antd/dist/antd.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <Input/>
+      </Fragment>
     );
   }
 }
---
+++ src/component/Input.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+
+class Input extends Component {
+  render() {
+    return (
+      <Button type="primary">Button</Button>
+    )
+  }
+}
+
+export default Input;
